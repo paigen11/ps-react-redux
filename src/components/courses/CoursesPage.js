5d59db2b1c822e595e4b3cd90b1909ceef905aee
Paige Niedringhaus <paigen11@gmail.com>
2019-04-06 16:33:42 -0400

    adding first form that will need redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
 
 export default class CoursesPage extends Component {
+  state = {
+    course: {
+      title: '',
+    },
+  };
+
+  handleChange = e => {
+    const course = { ...this.state.course, title: e.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
