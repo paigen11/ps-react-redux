7317a3c6d6d17f875c7b545ea1a7d20e533c8c9c
Paige Niedringhaus <paigen11@gmail.com>
2019-04-08 10:28:27 -0400

    redux store configured in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './components/App';
 import './index.css';
+import configureStore from './redux/configureStore';
+
+/* it can be useful to pass initial state into the store here
+ if you're server rendering or rehydrating the Redux store with 
+ data from local storage */
+const store = configureStore();
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById('app'),
 );
