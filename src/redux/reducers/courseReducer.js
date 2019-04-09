90ad94a0971a218d7bed6f1fecd4b136330289d7
Paige Niedringhaus <paigen11@gmail.com>
2019-04-09 15:12:54 -0400

    consolidating initial state for redux

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,7 +1,8 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
 // all reducers expect state and action as their arguments
-export default function courseReducer(state = [], action) {
+export default function courseReducer(state = initialState.courses, action) {
   switch (action.type) {
     case types.CREATE_COURSE:
       // state.push(action.course); // don't do this it mutates state
